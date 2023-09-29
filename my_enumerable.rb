ba5e85fb8752087e3b213f9cb1f948f72a1bface
zbahati <bahatizirimwabagabo@gmai.com>
2023-09-29 11:54:48 +0200

    Add rubocop.yml and fix rubocop linters errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
-
-# Class definition
 module MyEnumerable
   def all?(&block)
     each { |item| return false unless block.call(item) }
--- my_list.rb
+++ my_list.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# Class definition
 class MyList
   include MyEnumerable
 
